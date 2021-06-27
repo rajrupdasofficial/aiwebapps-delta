5832002d6501c372ba0ad5026e38030f7d1bc757
Rajrup Das <rajrupdasid@outlook.com>
2021-06-27 18:12:34 +0530

    All Graph Charts hasbeen added and More Bugs are smashed

--- AIweb/utils.py
+++ AIweb/utils.py
@@ -1,5 +1,49 @@
 import uuid
-
+from customers.models import Customer
+from profiles.models import Profile
+from io import BytesIO
+import matplotlib.pyplot as plt
+import base64
+import seaborn as sns
 def generate_code():
     code=str(uuid.uuid4()).replace('-','').upper()[:12]
     return code
+
+def get_salesman_from_id(val):
+    salesman=Profile.objects.get(id=val)
+    return salesman.user.username
+def get_customer_from_id(val):
+    customer=Customer.objects.get(id=val)
+    return customer
+
+
+def get_graph():
+
+    buffer=BytesIO()
+    plt.savefig(buffer,format='png')
+    buffer.seek(0)
+    image_png=buffer.getvalue()
+    graph=base64.b64encode(image_png)
+    graph=graph.decode('utf-8')
+    buffer.close()
+    return graph
+
+
+def get_chart(chart_type,data,**kwargs):
+    plt.switch_backend('AGG')
+    fig=plt.figure(figsize=(15,6))
+    if chart_type=='#1':
+        print('Bar Chart')
+        #plt.bar(data['transaction_id'],data['price'])
+        sns.barplot(x='transaction_id',y='price',data=data)
+    elif chart_type=='#2':
+        print('Pie Chart')
+        labels=kwargs.get('labels')
+        plt.pie(data=data,x='price',labels=labels)
+    elif chart_type=='#3':
+        plt.plot(data['transaction_id'],data['price'],color='red',marker='o',linestyle='dashed')
+    else:
+        print('Failed')
+    plt.tight_layout()
+    chart=get_graph
+    return chart
--- AIweb/views.py
+++ AIweb/views.py
@@ -4,19 +4,29 @@ from django.views.generic import ListView,DetailView
 from .models import Sale
 from .forms import  SalesSearchForm
 import pandas as pd
+from .utils import *
+from pandas.core.frame import DataFrame
 # Create your views here.
 def home_view(request):
     sales_df=None
     positions_df=None
+    merged_df=None
+    df=None
+    chart=None
     form=SalesSearchForm(request.POST or None)
     if request.method == 'POST':
         date_from=request.POST.get('date_form')
         date_to=request.POST.get('date_to')
         chart_type=request.POST.get('chart_type')
-        print(date_from,date_to,chart_type)
+        #print(date_from,date_to,chart_type)
         sale_qs=Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
         if len(sale_qs)>0:
             sales_df=pd.DataFrame(sale_qs.values())
+            sales_df['customer_id']=sales_df['customer_id'].apply(get_customer_from_id)
+            sales_df['salesman_id']=sales_df['salesman_id'].apply(get_salesman_from_id)
+            sales_df['created']=sales_df['created'].apply(lambda x: x.strftime('%Y-%m-%d'))
+            sales_df.rename({'customer_id':'customer','salesman_id':'salesman','id':'sales_id'},axis=1,inplace=True)
+            #sales_df['sales_id']=sales_df['id']
             positions_data=[]
             for sale in sale_qs:
                 for pos in sale.get_positions():
@@ -27,10 +37,17 @@ def home_view(request):
                         'price':pos.price,
                         'sales_id':pos.get_sales_id()
                     }
-                    positions_data.append(obj)       
-            sales_df=sales_df.to_html()
+                    positions_data.append(obj) 
+            
             positions_df=pd.DataFrame(positions_data)
-            positions_df=positions_df.to_html()
+            merged_df=pd.merge(sales_df,positions_df,on='sales_id')    
+            df=merged_df.groupby('transaction_id',as_index=False)['price'].agg('sum') 
+            chart=get_chart(chart_type,df,labels=df['transaction_id'].values)
+            print('chart',chart)
+            sales_df=sales_df.to_html()            
+            positions_df=positions_df.to_html() 
+            merged_df=merged_df.to_html()
+            df=df.to_html()
         else:
             print("There is no data")
 
@@ -40,6 +57,9 @@ def home_view(request):
         'form':form,
         'sales_df':sales_df,
         'positions_df':positions_df,
+        'merged_df':merged_df,
+        'df':df,
+        'chart':chart,
     }
     return render(request,'AIweb/home.html',context)
 
