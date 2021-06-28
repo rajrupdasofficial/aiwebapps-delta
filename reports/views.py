48c5bdc0a79a2439ae94d6819e596a85f50749ad
Rajrup Das <rajrupdasid@outlook.com>
2021-06-28 18:43:14 +0530

    critical bugs fixes and some performance enhancement

--- reports/views.py
+++ reports/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
 from profiles.models import Profile
 from django.http import JsonResponse
-
+from . forms import ReportForm
 from .utils import get_report_image
 from .models import Report
 # Create your views here.
 def create_report_view(request):
+    form = ReportForm(request.POST or None)
     if request.is_ajax():
-        name=request.POST.get('name')
-        remarks=request.POST.get('remarks')
+        #name=request.POST.get('name')
+        #remarks=request.POST.get('remarks')
+        
         image=request.POST.get('image')
-
         img=get_report_image(image)
-
         author=Profile.objects.get(user=request.user)
-        Report.objects.create(name=name,remarks=remarks,image=img, author=author)
+        if form.is_valid():
+            instance =form.save(commit=False)
+            instance.image=img
+            instance.author=author
+            instance.save()
+        #Report.objects.create(name=name,remarks=remarks,image=img, author=author)
         return JsonResponse({'msg':'send'})
     return JsonResponse({})
