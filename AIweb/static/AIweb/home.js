41de9130f188e86ba61a40131c055636e6849d87
Rajrup Das <rajrupdasid@outlook.com>
2021-06-28 19:27:53 +0530

    Internal Server Error Bug fixed  adding feature report list and detail page

--- AIweb/static/AIweb/home.js
+++ AIweb/static/AIweb/home.js
@@ -4,10 +4,21 @@ const reportBtn=document.getElementById('report-btn')
 const img=document.getElementById('img')
 const modalBody=document.getElementById('modal-body')
 const reportForm=document.getElementById('report-form')
+const alertBox=document.getElementById('alert-box')
 
 const reportName=document.getElementById('id_name')
 const reportRemarks=document.getElementById('id_remarks')
 const csrf= document.getElementsByName('csrfmiddlewaretoken')[0].value
+
+const handleAlerts=(type,msg)=>{
+    alertBox.innerHTML=`
+        <div class="alert alert-${type}" role="alert">
+            ${msg}
+        </div>
+    `
+}
+
+
 
 if (img){
 reportBtn.classList.remove('not-visible')
@@ -32,9 +43,11 @@ reportBtn.addEventListener('click', ()=>{
             data:formData,
             success: function(response){
                 console.log(response)
+                handleAlerts('success','report created')
             },
             error: function(error){
                 console.log(error)
+                handleAlerts('danger','500: Internal Server Error')
             },
             processData:false,
             contentType:false,
